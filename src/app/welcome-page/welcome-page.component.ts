b4f4cbb31464188eca3279a211592fef1a18c04b
tonymcaron <tonymcaron@gmail.com>
2026-01-02 16:14:15 -0700

    Update welcome-page design

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -24,6 +25,13 @@ export class WelcomePageComponent implements OnInit {
     this.dialog.open(UserLoginFormComponent, {
       // Assigning the dialog a width
       width: '280px'
+    });
+  }
+
+  // This is the function that will open the dialog when the movies button is clicked
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
     });
   }
 }
